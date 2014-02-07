0766122598130acddf232415b139a6e088668289
metaraine <raineorshine@gmail.com>
2014-02-07 07:41:58 -0700

    Initial setup for Restaurant UI.

--- js/JS20-restaurant-ui/food.js
+++ js/JS20-restaurant-ui/food.js
@@ -23,10 +23,6 @@ FoodItem.prototype.toString = function() {
 	])
 }
 
-FoodItem.prototype.create = function() {
-	return '<div class="food-item">{name}</div>'.supplant(this);
-}
-
 // Plate
 function Plate(name, description, price, ingredients) {
 	this.name = name;
@@ -41,7 +37,7 @@ Plate.prototype.toString = function() {
 		'Vegan: {2}',
 		'Gluten Free: {3}',
 		'Citrus Free: {4}',
-		'Ingredients: \n  {5}'
+		'Ingredients: \n{5}'
 	].join('\n').supplant([
 		this.name,
 		this.description,
@@ -53,66 +49,16 @@ Plate.prototype.toString = function() {
 }
 
 Plate.prototype.isVegan = function() {
-	for(var i=0, len=this.ingredients.length; i<len; i++) {
-		if(!this.ingredients[i].vegan) {
-			return false;
-		}
-	}
-	return true;
+	return and(pluck(this.ingredients, 'vegan'));
 }
 
 Plate.prototype.isGlutenFree = function() {
-	for(var i=0, len=this.ingredients.length; i<len; i++) {
-		if(!this.ingredients[i].glutenFree) {
-			return false;
-		}
-	}
-	return true;
+	return and(pluck(this.ingredients, 'glutenFree'));
 }
 
 Plate.prototype.isCitrusFree = function() {
-	for(var i=0, len=this.ingredients.length; i<len; i++) {
-		if(!this.ingredients[i].citrusFree) {
-			return false;
-		}
-	}
-	return true;
+	return and(pluck(this.ingredients, 'citrusFree'));
 }
-
-Plate.prototype.create = function() { 
-	return '<div class="plate"><h3>{name}</h3><h4>{description}</h4><ul>{0}</ul></div>'
-		.supplant(this)
-		.supplant([
-			RJS.map(
-				RJS.map(RJS.pluck(this.ingredients, 'create')), 
-				RJS.rcurry(RJS.bookend, ['<li>', '</li>'])
-			).join('')
-		]);
-}
-
-/*
-// ADVANCED: Here is how you can abstract the above logic to eliminate the looping redundancy
-Plate.prototype.isTrueOfAllIngredients = function(prop) {
-	for(var i=0, len=this.ingredients.length; i<len; i++) {
-		if(!this.ingredients[i][prop]) {
-			return false;
-		}
-	}
-	return true;
-}
-
-Plate.prototype.isVegan = function() {
-	return this.isTrueOfAllIngredients('vegan');
-}
-
-Plate.prototype.isGlutenFree = function() {
-	return this.isTrueOfAllIngredients('glutenFree');
-}
-
-Plate.prototype.isCitrusFree = function() {
-	return this.isTrueOfAllIngredients('citrusFree');
-}
-*/
 
 // Drink
 function Drink(name, description, price, ingredients) {
@@ -125,15 +71,13 @@ function Drink(name, description, price, ingredients) {
 Drink.prototype.toString = function() {
 	return [
 		'{0} - {1}',
-		'Ingredients: \n  {2}'
+		'Ingredients: \n{2}'
 	].join('\n').supplant([
 		this.name,
 		this.description,
 		pluck(this.ingredients, 'name').join('\n  ')
 	])
 }
-
-Drink.prototype.create = Plate.prototype.create;
 
 // Order
 function Order(plates) {
@@ -151,12 +95,6 @@ function Menu(plates) {
 
 Menu.prototype.toString = function() {
 	return this.plates.join('\n\n');
-}
-
-Menu.prototype.create = function() {
-	return '<ul class="list-unstyled">{0}</ul>'.supplant([
-		RJS.map(RJS.pluck(this.plates, 'create')).join('')
-	])
 }
 
 // Restaurant
