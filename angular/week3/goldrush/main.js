a6540b0e10683d1f93b65a99ad69d0e95f26a94c
rserota <raphael.serota@gmail.com>
2016-04-22 11:27:18 -0600

    ...

--- angular/week3/goldrush/main.js
+++ angular/week3/goldrush/main.js
@@ -2,26 +2,23 @@
 var mainController = function($scope){
     console.log('controller is up')
     $scope.addingANote = false
-    $scope.whichNote = 0
     $scope.clickSpot = [0,0]
     $scope.markers = []
+    $scope.noteInput = ''
+
     $scope.addMarker = function(event){
-        // console.log(event.pageY)
-        // console.log(event.pageX)
-        $scope.addingANote = true
         $scope.clickSpot = {
             x : event.pageX / document.body.clientWidth * 100,
             y : event.pageY / document.body.clientHeight * 100
         }
         $scope.markers.push($scope.clickSpot)
-        console.log($scope.clickSpot)
         $scope.whichNote = $scope.markers.length -1
+        $scope.addingANote = true
     }
     $scope.addNote = function(){
-        $scope.markers[$scope.whichNote].note = $scope.noteInput
+        $scope.markers[$scope.markers.length -1].note = $scope.noteInput
         $scope.addingANote = false
         $scope.noteInput = ''
-        console.log($scope.markers)
     }
 
     $scope.removeMarker = function(index){
